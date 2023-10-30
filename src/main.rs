628acfed85edb3792df28fb5f3bea44a87fda939
Marshall Bruner <brunerm99@gmail.com>
2023-10-30 23:38:29 +0200

    Started going through the unofficial docs and applying to the character game

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+mod character;
+mod player;
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
+        .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
+        .run();
+}
+
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("player_spritesheet.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 46.0), 6, 8, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let animation_indices = AnimationIndices {
+        first: 42,
+        last: 47,
+    };
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+}
---
+++ src/player.rs
@@ -0,0 +1,61 @@
+use crate::character::Character;
+use bevy::prelude::*;
+
+#[derive(Resource)]
+pub enum CharacterMovements {
+    IdleLeft,
+    IdleDown,
+    IdleUp,
+    IdleRight,
+    WalkLeft,
+    WalkDown,
+    WalkUp,
+    WalkRight,
+    RunLeft,
+    RunDown,
+    RunUp,
+    RunRight,
+}
+
+#[derive(Component)]
+pub struct Health {
+    pub hp: i8,
+}
+
+impl Health {
+    pub fn new() -> Self {
+        Health { hp: 100 }
+    }
+}
+
+#[derive(Component)]
+pub struct PlayerBundle {
+    pub name: String,
+    pub health: Health,
+    pub sprite: SpriteSheetBundle,
+    pub character: Character,
+}
+
+impl PlayerBundle {
+    pub fn new(
+        name: &str,
+        sprite_sheet_path: &str,
+        asset_server: Res<AssetServer>,
+        mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    ) -> Self {
+        let texture_handle = asset_server.load(sprite_sheet_path);
+        let texture_atlas =
+            TextureAtlas::from_grid(texture_handle, Vec2::new(32.0, 46.0), 6, 8, None, None);
+        let texture_atlas_handle = texture_atlases.add(texture_atlas);
+        PlayerBundle {
+            name: name.to_owned(),
+            health: Health::new(),
+            sprite: SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(0), // TODO: Change to default player state
+                transform: Transform::from_scale(Vec3::splat(6.0)),
+                ..default()
+            },
+        }
+    }
+}
